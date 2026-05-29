497ea35b86114b9957c7dd99677731115a4ce198
Yash Gangwar <103266801+YashGangwar3956@users.noreply.github.com>
2026-05-29 14:13:02 +0530

    Time: 50 ms (5.88%), Space: 57.4 MB (31.76%) - LeetHub

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minElement = function(nums) {
+    const digitSum = (num) => {
+        let sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+        return sum;
+    };
+
+    let minVal = Infinity;
+
+    for (let num of nums) {
+        minVal = Math.min(minVal, digitSum(num));
+    }
+
+    return minVal;
+};
