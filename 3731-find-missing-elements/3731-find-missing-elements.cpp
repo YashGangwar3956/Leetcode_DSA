334a6c465baefac2463f85418d850692460bfdd3
Yash Gangwar <103266801+YashGangwar3956@users.noreply.github.com>
2026-08-04 09:33:52 +0100

    Time: 0 ms (100%), Space: 32.3 MB (54.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        vector<int> a;
+         vector<int> a;
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size() - 1; i++) {
