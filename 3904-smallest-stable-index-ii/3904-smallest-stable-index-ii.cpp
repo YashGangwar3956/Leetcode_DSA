c18e2f17611dc607870d012eabbbb140c9bc6463
Yash Gangwar <103266801+YashGangwar3956@users.noreply.github.com>
2026-09-05 18:13:10 +0530

    Time: 5 ms (83.31%), Space: 202.6 MB (85.02%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n == 0) return -1;
+        vector<int> suffMin(n);
+        suffMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            suffMin[i] = min(suffMin[i + 1], nums[i]);
+        }
+        int currentMax = nums[0];
+        for (int i = 0; i < n; ++i) {
+            currentMax = max(currentMax, nums[i]);
+            if ((long long)currentMax - suffMin[i] <= (long long)k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
