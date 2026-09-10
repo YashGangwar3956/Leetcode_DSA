e2ba9d3dba0e2abe657890bdbec4754ba5daf59c
Yash Gangwar <103266801+YashGangwar3956@users.noreply.github.com>
2026-09-11 02:22:10 +0530

    Time: 541 ms (79.92%), Space: 130.3 MB (9.54%) - LeetHub

--- 4041-minimum-operations-to-form-subset-sum-ii/4041-minimum-operations-to-form-subset-sum-ii.cpp
+++ 4041-minimum-operations-to-form-subset-sum-ii/4041-minimum-operations-to-form-subset-sum-ii.cpp
@@ -6,55 +6,41 @@ public:
         dp[0] = 0;
         for (long long x : nums) {
             vector<long long> cost(sum + 1, INF);
-
             vector<pair<int, int>> options;
-
             long long curr = x;
             int down = 0;
             while (curr > 0) {
-
                 long long val = curr;
                 int up = 0;
                 while (val <= sum) {
-
                     if (cost[val] == INF) {
                         options.push_back({(int)val, down + up});
                     }
-
                     cost[val] = min(
                         cost[val],
                         (long long)down + up
                     );
-
                     if (val > sum / 2)
                         break;
-
                     val *= 2;
                     up++;
                 }
-
                 curr /= 2;
                 down++;
             }
             vector<long long> next = dp;
-
             for (int s = 0; s <= sum; s++) {
-
                 if (dp[s] == INF)
                     continue;
-
                 for (auto [value, operations] : options) {
-
                     if (s + value > sum)
                         continue;
-
                     next[s + value] = min(
                         next[s + value],
                         dp[s] + operations
                     );
                 }
             }
-
             dp = move(next);
         }
         if(dp[sum] == INF){
